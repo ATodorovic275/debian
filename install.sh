d985e28c832eea202d64ddd8e9c7b191e117c963
ATodorovic275 <60385383+ATodorovic275@users.noreply.github.com>
2025-12-16 21:12:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
-sudo nala install cinnamon fastfetch geany network-manager-applet gsmartcontrol unzip gnome-power-manager power-profiles-daemon gnome-system-monitor psensor ristretto qbittorrent 
+sudo nala install cinnamon curl git fastfetch geany network-manager-applet gsmartcontrol unzip gnome-power-manager power-profiles-daemon gnome-system-monitor psensor ristretto qbittorrent 
+curl -fsS https://dl.brave.com/install.sh | sh
+sudo reboot
