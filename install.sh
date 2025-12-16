53a4b7687e0b3579b28d9d6e5f98b1722760a5d1
ATodorovic275 <60385383+ATodorovic275@users.noreply.github.com>
2025-12-16 20:25:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-sudo nala install cinnamon lxdm fastfetch geany network-manager-applet gsmartcontrol unzip gnome-power-manager power-profiles-daemon gnome-system-monitor psensor ristretto qbittorrent 
+sudo nala install cinnamon fastfetch geany network-manager-applet gsmartcontrol unzip gnome-power-manager power-profiles-daemon gnome-system-monitor psensor ristretto qbittorrent 
